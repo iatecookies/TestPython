55fa2f9f6a9b436fe1130ce5d715ea08890c745b
s1719890 <j.f.cheung@umail.leidenuniv.nl>
2020-10-09 15:05:54 +0200

    Explain callback and how to call object from class within class

--- test.py
+++ test.py
@@ -1,30 +1,101 @@
-#!/usr/local/bin/python3.8.2
-# Simple pygame program
+import time
 
-# Import and initialize the pygame library
-import pygame
-pygame.init()
+# TEST CLASS -------------------------------------------------------------------
+# import menu
+class Test ():
+    def __init__ (self):
+        self.framerate = 10
+        self.status = 'b'
 
-# Set up the drawing window
-screen = pygame.display.set_mode([500, 500])
+    def start (self):
+        self.gameLoop()
 
-# Run until the user asks to quit
-running = True
-while running:
+    def game (self):
+        self.framerate = 10
+        print('Playing the game')
 
-    # Did the user click the window close button?
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    def startMenu (self):
+        self.framerate = 1
+        menu = Menu('Start Menu')
+        cont = menu.addButton('Continue game')
 
-    # Fill the background with white
-    screen.fill((255, 255, 255))
+        if cont.setColor('ret'):
+            print('Color changed')
+        else:
+            print('Color not changed')
 
-    # Draw a solid blue circle in the center
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+        hs = menu.addButton('Highscores')
+        quit = menu.addButton('Quit game')
 
-    # Flip the display
-    pygame.display.flip()
+        def aFunction ():
+            self.status = 'b'
 
-# Done! Time to quit.
-pygame.quit()
+        cont.click(aFunction)
+
+    def highscoreMenu (self):
+        self.framerate = 1
+        print('Highscore menu')
+
+
+
+    def gameLoop (self):
+        while True:
+            # Timing van de framerate
+            time.sleep(1 / self.framerate)
+            # Speel het spel
+            if self.status == 'a':
+                self.game()
+            # Het start menu
+            elif self.status == 'b':
+                self.startMenu()
+            # Het highscore menu
+            elif self.status == 'c':
+                self.highscoreMenu()
+            else:
+                print("Something else...")
+
+# MENU CLASS -------------------------------------------------------------------
+# import button
+class Menu ():
+    def __init__ (self, title='No title'):
+        self.title = title
+        self.buttons = []
+
+    def addButton (self, title):
+        button = Button(title)
+        self.buttons.append(button)
+        return button
+
+
+
+# BUTTON CLASS -----------------------------------------------------------------
+
+class Button ():
+    def __init__ (self, title):
+        self.title = title
+        self.color = 'green'
+        self.colorsAllowed = ['red', 'green', 'blue']
+
+    def click (self, callback=None):
+        # Standard action might be logging the button click
+        print('You pressed the button: \'' + self.title + '\'.')
+        if callback != None:
+            callback()
+
+    def setColor (self, value):
+        if value in self.colorsAllowed:
+            self.color = value
+            return True
+        return False
+
+    def getColor (self):
+        return self.color
+
+
+
+
+# TESTING ----------------------------------------------------------------------
+
+game = Test()
+#game.status = 'Dikke aap'
+game.start()
